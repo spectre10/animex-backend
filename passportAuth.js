de002755a4cde6d25aca19aacbd5add2a087c9d1
spectre10 <shyamthakkar001@gmail.com>
2023-06-25 19:01:51 +0530

    updated auth url

--- passportAuth.js
+++ passportAuth.js
@@ -22,7 +22,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/auth/google/callback",
+      callbackURL: "/auth/google/callback",
       // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
       // passReqToCallback:true
     },
